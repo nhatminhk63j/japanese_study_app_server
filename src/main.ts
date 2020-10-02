5350561a0688618a21fb4f691b722d9f300a2121
nhat.ns <nhat.ns@teko.vn>
2020-10-02 15:56:31 +0700

    wrap data response by response-transfromer-interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationConfig } from './configs/validation';
+import { ResponseTransformInterceptor } from './interceptors/response.transform.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalInterceptors(new ResponseTransformInterceptor());
+
+  app.useGlobalPipes(new ValidationPipe(ValidationConfig));
+
+  const port = process.env.HTTP_PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
