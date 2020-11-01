4f96b1305a053f15fcc813e32270aadb7c9a43a9
nhat.ns <nhat.ns@teko.vn>
2020-11-01 17:01:32 +0700

    refactor login by google

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,11 +3,18 @@ import { map } from 'rxjs/operators';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from './../users/user.service';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { User } from 'src/db/entities/user.entity';
 import * as bcrypt from 'bcrypt';
-import { google } from 'googleapis';
 import fetch from 'node-fetch';
+
+const GOOGLE_OAUTH_URL =
+  'https://www.googleapis.com/oauth2/v2/userinfo?access_token=';
+const FACEBOOK_OAUTH_URL = 'https://graph.facebook.com/me?access_token=';
 
 @Injectable()
 export class AuthService {
@@ -48,31 +55,21 @@ export class AuthService {
   }
 
   async getUserByAccessTokenGoogle(accessToken: string): Promise<User> {
-    const endpointUrl =
-      'https://www.googleapis.com/oauth2/v2/userinfo?access_token=' +
-      accessToken;
+    const endpointUrl = GOOGLE_OAUTH_URL + accessToken;
     const userGoogle: UserGoogle = await fetch(endpointUrl).then(res =>
       res.json(),
     );
 
     if ((userGoogle as any).error) {
-      throw new UnauthorizedException('Unauthorized by google');
+      throw new UnauthorizedException('Access token is not valid');
     }
 
     const user = await this.userService.findByEmail(userGoogle.email);
 
     if (!user) {
-      const newUser: CreateUserDto = {
-        firstName: userGoogle.family_name,
-        lastName: userGoogle.given_name,
-        avatarUrl: userGoogle.picture,
-        email: userGoogle.email,
-        password: userGoogle.email,
-        password_confirmation: userGoogle.email,
-        isActive: true,
-      };
-      return this.userService.store(newUser);
+      throw new NotFoundException('User is not exist');
     }
+
     return user;
   }
 }
@@ -83,12 +80,4 @@ interface UserGoogle {
   given_name: string;
   family_name: string;
   picture: string;
-}
-
-interface ErrorGoogle {
-  error: {
-    code: number;
-    message: string;
-    status: string;
-  };
 }
